d896532b6a3105e82cea0a4143b50def7456b431
cdifranco1 <59182391+cdifranco1@users.noreply.github.com>
2020-02-05 12:20:22 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+let menuIcon = document.querySelector(".menu-icon");
+console.log(menuIcon)
+let dropdown = document.querySelector(".dropdown");
+console.log(dropdown)
+menuIcon.addEventListener("click", toggleDisplay)
+
+function toggleDisplay(){
+  if (dropdown.style.display == "" || dropdown.style.display == 'none'){
+    dropdown.style.display = 'flex'
+  } else {
+    dropdown.style.display = 'none'
+  }
+};
