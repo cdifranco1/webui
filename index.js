ba9c5f60ea523286db485fb600a3ac4dc675dad7
Charlie DiFranco <charlie.difranco2@gmail.com>
2020-02-05 14:17:49 -0600

    completed project

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-let menuIcon = document.querySelector(".menu-icon");
-console.log(menuIcon)
-let dropdown = document.querySelector(".dropdown");
-console.log(dropdown)
-menuIcon.addEventListener("click", toggleDisplay)
 
-function toggleDisplay(){
-  if (dropdown.style.display == "" || dropdown.style.display == 'none'){
-    dropdown.style.display = 'flex';
-  } else {
-    dropdown.style.display = 'none'
-  }
+window.onload = function(){
+  let menuIcon = document.querySelector(".menu-icon");
+  let dropdown = document.querySelector(".dropdown");
+  let mainNav = document.querySelector(".main-nav");
+  menuIcon.addEventListener("click", toggleDisplay)
+
+  function toggleDisplay(){
+    if (dropdown.style.display == "" || dropdown.style.display == 'none'){
+      dropdown.style.display = 'flex';
+    } else {
+      dropdown.style.display = 'none';
+    }
+  };
 };
