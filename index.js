fb3b58290f3c5883e253804b07a96252c509ce8f
Charlie DiFranco <charlie.difranco2@gmail.com>
2020-02-07 11:20:26 -0600

    made minor tweak to dropdown

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ window.onload = function(){
   let menuIcon = document.querySelector(".menu-icon");
   let dropdown = document.querySelector(".dropdown");
   let mainNav = document.querySelector(".main-nav");
-  menuIcon.addEventListener("click", toggleDisplay)
+  menuIcon.addEventListener("click", toggleDisplay);
+  
 
   function toggleDisplay(){
     if (dropdown.style.display == "" || dropdown.style.display == 'none'){
@@ -12,4 +13,13 @@ window.onload = function(){
       dropdown.style.display = 'none';
     }
   };
+
+  const removeDropdown = function(e){
+    width = e.target.outerWidth;
+    if (width > 500){
+      dropdown.style.display = 'none';
+    }
+  };
+
+  window.addEventListener("resize", removeDropdown);
 };
